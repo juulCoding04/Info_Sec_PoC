f30fbadb71d84711c4f56e91d758c33add162a38
Juul <juul20018@gmail.com>
2026-04-16 22:42:27 +0200

    adding function that need to be implemented in keys.py

---
+++ crypto/keys.py
@@ -0,0 +1,17 @@
+# imports here
+
+# generate keypair
+def generate_keypair():
+    pass
+
+# save keypair
+def save_keypair(private_key, public_key, dir):
+    pass
+
+# load private key
+def load_private_key(path):
+    pass
+
+# load public key
+def load_public_key(path):
+    pass
