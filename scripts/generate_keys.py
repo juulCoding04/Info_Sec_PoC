e3be75727c3c139db859bd82695664472e0a6700
Juul <juul20018@gmail.com>
2026-04-28 11:14:17 +0200

    feat(issuers): adding a trusted_issuers.json and added a script to generate keys

---
+++ scripts/generate_keys.py
@@ -0,0 +1,19 @@
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from crypto.keys import generate_keypair, save_keypair
+
+parties = [
+    "issuer/issuer_keys/ugent",
+    "issuer/issuer_keys/belgian_government",
+]
+
+for dir in parties:
+    if os.path.exists(f"{dir}/private_key.pem"):
+        print(f"Keys already exist for {dir}, skipping.")
+        continue
+
+    priv, pub = generate_keypair()
+    save_keypair(priv, pub, dir)
+    print(f"Keys have been saved to {dir}")
